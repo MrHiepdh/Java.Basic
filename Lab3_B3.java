af5ff30bdf94962dc73710aeafa79c91327cab0b
MrHiep01 <nguyencaohiep.ecorp@gmail.com>
2020-09-04 17:42:04 +0700

    Update Lab3_B3.java

--- Lab3_B3.java
+++ Lab3_B3.java
@@ -1,40 +1,31 @@
-package Junior;
+package junior;
 import java.util.Scanner;
-public class Lab3_B3 {
-    public static void main(String []args){
+public class Lab3_B3{
+    public static void main(String [] args){
         Scanner s =new Scanner(System.in);
-        int i,j,n;
-        int flag;
+        int n;
         System.out.println("Enter the high of triangle : ");
-        n=s.nextInt();
-        for(i=1;i<=n;i++){
-            flag=i%10;
-            for(j=0;j<=2*n-1;j++){
-                if(j>=n-i&&j<n){
-                    if(flag>9){
-                        flag=0;
-                    }
-                    System.out.printf("%d ",flag);
-                    flag++;
-                }
-                else if(j==n+1&&i!=1){
-                    flag-=2;
-                    System.out.printf("%d ",flag);
-                    flag--;
-                }
-                else if(j>n+1&&j<n+i){
-                    if(flag<0){
-                        flag=9;
-                    }
-                    System.out.printf("%d ",flag);
-                    flag--;
-                }
-                else if(j>=n+i||j<n-i){
-                    System.out.print("  ");
-                }
-            }
-            System.out.print("\n");
-        }  
+        n = s.nextInt();
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n - i; j++)
+            System.out.printf("  ");
+        for (int j = i; j <= 2 * i - 1; j++)
+        {
+            if (j < 10)
+            System.out.printf("%d ",j);
+            else 
+            System.out.printf("%d ", j - 10);
+        }
+        for (int j = 2 * i - 2; j >= i; j--)
+        {
+            if (j < 10)
+            System.out.printf("%d ",j);
+            else 
+            System.out.printf("%d ", j - 10);
+        }
+        System.out.printf("\n");
     }
+    s.close();
 }
-
+}
